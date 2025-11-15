c3ac3e5a6c4179342fe8758e450d0439338ecf5e
Fabian Urbina <fabian2357@gmail.com>
2025-11-15 16:59:57 -0300

    removed auth, header

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -13,10 +13,6 @@ export default function Home() {
   return (
     <>
       <Hero />
-      <BusinessCategories />
-      <FeaturesPlanet />
-      <LargeTestimonial />
-      <Cta />
     </>
   );
 }
